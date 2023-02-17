c51a83e60ac0cde28621c0d94aed637f3e0126e7
Vinayaka0204 <vinayakahegde94@gmail.com>
2023-02-17 09:32:08 +0530

    hello.cpp upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, I am PES2UG20CS923\n"
+  cout << "I have successfully built and run\n";
+  return 0;
+}
